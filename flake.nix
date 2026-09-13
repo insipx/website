d50a36c67551d9045eacad4c008be9546d433213
Andrew Plaza <github@andrewplaza.dev>
2026-09-12 20:07:35 -0400

    nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A flake for developing and building my personal website";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.website = pkgs.stdenv.mkDerivation {
+          pname = "website";
+          version = "2026-09-12";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.zola ];
+          buildPhase = "zola build";
+          installPhase = "cp -r public $out";
+        };
+        defaultPackage = self.packages.${system}.website;
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            zola
+          ];
+        };
+      }
+    );
+}
